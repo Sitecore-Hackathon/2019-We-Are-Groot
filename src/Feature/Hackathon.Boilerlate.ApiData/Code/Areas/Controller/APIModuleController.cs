7d75a5e05165e084eb46a6a19edcf11cc567c40f
Epsilon-Bharath <bharath.cn@epsilon.com>
2019-03-02 17:51:15 +0530

    Updated API Connector

--- src/Feature/Hackathon.Boilerlate.ApiData/Code/Areas/Controller/APIModuleController.cs
+++ src/Feature/Hackathon.Boilerlate.ApiData/Code/Areas/Controller/APIModuleController.cs
@@ -1,9 +1,14 @@
-﻿using Sitecore.Mvc.Controllers;
+﻿using Hackathon.Boilerlate.Api.Areas.Model;
+using Newtonsoft.Json;
+using Sitecore.Data.Fields;
+using Sitecore.Mvc.Controllers;
 using Sitecore.Mvc.Presentation;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Web;
+using System.Net;
+using System.Text;
 using System.Web.Mvc;
 
 namespace Hackathon.Boilerlate.Api.Areas.Controller
@@ -13,8 +18,65 @@ namespace Hackathon.Boilerlate.Api.Areas.Controller
         // GET: Controller/APIModule
         public override ActionResult Index()
         {
-            var datasource = RenderingContext.Current.Rendering.Item;
-            return View("~/Views/APIModule.cshtml",datasource);
+            var APIData = RenderingContext.Current.Rendering.Item;
+            APIModule datasource = new APIModule();
+            datasource.APIUrl = APIData.Fields["URL"].ToString();
+            datasource.SelectedHeaderValues = APIData.Fields["Select Header Parameters"];
+            datasource.HeaderValues = selectedMultilistValues(datasource.SelectedHeaderValues);
+            datasource.SelectedInputList = APIData.Fields["Select Input Parameters"];
+            datasource.InputParameters = selectedMultilistValues(datasource.SelectedInputList);
+
+            var apiResult = InvokeJson<dynamic>(datasource);
+
+            return View("~/Views/APIModule.cshtml", apiResult);
+        }
+
+        public IEnumerable<InputParams> selectedMultilistValues(MultilistField data)
+        {
+            IEnumerable<InputParams> selectedInputs = data.GetItems().Select(x => new InputParams
+            {
+                Key = x.Fields["Key"].ToString(),
+                Value = x.Fields["Value"].ToString()
+            });
+            return selectedInputs;
+        }
+
+        public static T InvokeJson<T>(APIModule requestData) where T : class
+        {
+            try
+            {
+                Uri uri = new Uri(requestData.APIUrl);
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+                request.Method = "POST";
+                request.ContentType = "application/json";
+                foreach(var headerInput in requestData.HeaderValues)
+                {
+                    request.Headers.Add(headerInput.Key, headerInput.Value);
+                }
+                
+                string sb = JsonConvert.SerializeObject(requestData.InputParameters);
+                byte[] data = Encoding.ASCII.GetBytes(sb);
+                Stream newStream = request.GetRequestStream();
+                newStream.Write(data, 0, data.Length);
+                newStream.Close();
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                if (stream != null)
+                {
+                    StreamReader sr = new StreamReader(stream);
+                    string jsonResponse = sr.ReadToEnd();
+                    sr.Close();
+                    return (T)JsonConvert.DeserializeObject(jsonResponse, typeof(T));
+                }
+                return null;
+            }
+            catch (Exception e)
+            {
+                Sitecore.Diagnostics.Log.Error($"APIModule: Exception '{e.Message}' - '{e.StackTrace}'", e);
+                Console.WriteLine(e.Message);
+                return null;
+            }
         }
     }
 }
--- src/Feature/Hackathon.Boilerlate.ApiData/Code/Areas/Model/APIModule.cs
+++ src/Feature/Hackathon.Boilerlate.ApiData/Code/Areas/Model/APIModule.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sitecore.Data.Fields;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,16 @@ namespace Hackathon.Boilerlate.Api.Areas.Model
 {
     public class APIModule
     {
+        public String APIUrl { get; set; }
+        public MultilistField SelectedHeaderValues { get; set; }
+        public IEnumerable<InputParams> HeaderValues { get; set; }
+        public MultilistField SelectedInputList { get; set; }
+        public IEnumerable<InputParams> InputParameters { get; set; }
+    }
+
+    public class InputParams
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
     }
 }

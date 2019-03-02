f63a36800d1ef61cb5ce01a47535568339c97ecb
Epsilon-Bharath <bharath.cn@epsilon.com>
2019-03-02 11:28:47 +0530

    Added code Components

---
+++ src/Feature/Hackathon.Boilerlate.ApiData/Code/Areas/Controller/APIModuleController.cs
@@ -0,0 +1,20 @@
+﻿using Sitecore.Mvc.Controllers;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hackathon.Boilerlate.Api.Areas.Controller
+{
+    public class APIModuleController : SitecoreController
+    {
+        // GET: Controller/APIModule
+        public override ActionResult Index()
+        {
+            var datasource = RenderingContext.Current.Rendering.Item;
+            return View("~/Views/APIModule.cshtml",datasource);
+        }
+    }
+}
---
+++ src/Feature/Hackathon.Boilerlate.ApiData/Code/Areas/Model/APIModule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hackathon.Boilerlate.Api.Areas.Model
+{
+    public class APIModule
+    {
+    }
+}
